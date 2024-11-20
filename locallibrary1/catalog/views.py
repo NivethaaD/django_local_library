ff9dd9edb6b5a4ba69d24c207a327fe709ccc673
NivethaaD <nivethaad3@gmail.com>
2024-11-20 18:44:24 +0530

    Completed listview and detail view

--- locallibrary1/catalog/views.py
+++ locallibrary1/catalog/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'catalog/index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name="book_list.html"
+    paginate_by = 10
+   
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        # for content in context:
+        #     print(content)
+        return context
+    
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+from django.shortcuts import get_object_or_404
+
+# def book_detail_view(request, primary_key):
+#     book = get_object_or_404(Book, pk=primary_key)
+#     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template = "author_list.html"
+    context_object_name = "authors"
+
+    def get_context_data(self,**kwargs):
+
+        context = super(AuthorListView,self).get_context_data(**kwargs)
+        return context
+
+
+def author_detail_view(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, 'catalog/author_detail.html',context={'author' : author})
--- locallibrary1/locallibrary1/urls.py
+++ locallibrary1/locallibrary1/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
